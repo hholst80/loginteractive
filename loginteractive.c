4f95614879adc3c0efac04a330a3b0b5a1d50582
Henrik Holst <henrik.holst@dice.se>
2014-04-07 13:22:16 +0200

    Code cleanup

--- loginteractive.c
+++ loginteractive.c
@@ -15,63 +15,58 @@
 #include <sys/time.h>
 #define __USE_GNU
 #include <dlfcn.h>
+#include <unistd.h>
 
 static ssize_t (*real_read)(int,void*,size_t) = NULL;
 static int (*real_select)(int nfds, fd_set *restrict readfds, fd_set *restrict writefds, fd_set *restrict errorfds, struct timeval *restrict timeout) = NULL;
+static ssize_t (*real_write)(int,const void*,size_t) = NULL;
 
 static int stdindata = 0;
-static int stdindata_eof = 0;
 
 static void init(void)
 {
 	real_read = dlsym(RTLD_NEXT, "read");
-	if (NULL == real_read) {
+	if (!real_read)
 		fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
-	}
 	real_select = dlsym(RTLD_NEXT, "select");
-	if (NULL == real_select) {
+	if (!real_select)
 		fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
-	}
-	if (getenv("STDIN")) {
+	real_write = dlsym(RTLD_NEXT, "write");
+	if (!real_write)
+		fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
+	if (getenv("STDIN"))
 		stdindata = open(getenv("STDIN"),O_RDONLY);
-	} else {
+	else
 		stdindata = open("stdin.txt",O_RDONLY);
-	}
-	if (stdindata < 0) {
+	if (stdindata < 0)
 		perror("open");
-	}
 }
 
 int select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds, fd_set *restrict errorfds, struct timeval *restrict timeout)
 {
-	int count = 0;
-	if (!real_select) {
+	if (!real_select)
 		init();
-	}
-	if (!stdindata_eof) {
-		FD_SET(0,readfds);
-		return 1;
-	}
-	return real_select(nfds,readfds,writefds,errorfds,timeout);
+	if (stdindata < 0)
+		return real_select(nfds,readfds,writefds,errorfds,timeout);
+	FD_SET(0,readfds);
+	return 1;
 }
 
-ssize_t read(int fd, char *buffer, size_t length)
+ssize_t read(int filedes, void *buffer, size_t size)
 {
-	ssize_t count = 0;
-	if (real_read==NULL) {
+	if (!real_read)
 		init();
-	}
-	if (fd!=0) {
-		return real_read(fd,buffer,length);
-	}
-	while (count < length) {
-		int rc = real_read(stdindata,buffer+count,1);
+	if (stdindata < 0 || filedes != 0)
+		return real_read(filedes,buffer,size);
+	ssize_t count = 0;
+	while (count < size) {
+		ssize_t rc = real_read(stdindata,buffer+count,1);
 		if (rc == 0) {
-			stdindata_eof = 1;
+			close(stdindata);
+			stdindata = -1;
 			break;
 		}
-		++count;
-		if (buffer[count] == '\n')
+		if (((char*)buffer)[count++] == '\n')
 			break;
 	}
 	return count;
